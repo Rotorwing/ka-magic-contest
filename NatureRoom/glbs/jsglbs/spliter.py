7c4daa57e721652d4550c2a8a3e7ffc0cac8eda4
Jacob <steljaco@isu.edu>
2022-10-27 21:58:51 -0600

    made splits js files

--- NatureRoom/glbs/jsglbs/spliter.py
+++ NatureRoom/glbs/jsglbs/spliter.py
@@ -5,6 +5,6 @@ file.close();
 for n in range(10):
     linewrite = line[int(len(line)/10*n):int(len(line)/10*(n+1))]
     linewrite = "window.NatureRoomHDv7brokenUVs_converted += \"".format(n)+linewrite+"\";"
-    file2 = open("/Users/admin/Documents/3DKhan/MagicContest/NatureRoom/glbs/jsglbs/NatureRoom_{}.txt".format(n), 'w');
+    file2 = open("/Users/admin/Documents/3DKhan/MagicContest/NatureRoom/glbs/jsglbs/NatureRoom_{}.js".format(n), 'w');
     file2.write(linewrite);
     file2.close();
